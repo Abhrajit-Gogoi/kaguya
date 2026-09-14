5c9f44a1e165d56f0f0a3381de5e7abe373646f1
abhrajit-gogoi <abhrajit.gb@gmail.com>
2026-09-14 22:43:24 +0530

    Fix module specifier resolution with importmap and esnext target

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/kaguya/',
+  build: {
+    target: 'esnext'
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext'
+    }
+  },
   worker: {
     format: 'es'
   }
